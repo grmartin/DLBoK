477e817a4193c70de342cef20c9c7d9cbc120e76
Glenn R. Martin <grmartin@programmer.net>
2019-03-30 20:16:36 -0400

    Processing all Pixels in to Data Chunks.

--- examples/jpeg-get-pixels.ts
+++ examples/jpeg-get-pixels.ts
@@ -4,15 +4,13 @@ import {IFile, IImage, File, Image} from './pbuff';
 const fs = require('fs');
 const path = require('path');
 
-const flatbuffers = require('flatbuffers').flatbuffers;
-
 interface RGB {
     r: number;
     g: number;
     b: number;
 }
 
-async function processFile(file: string): Promise<[string, number[]]> {
+async function processFile(prefix: string, file: string): Promise<[string, number[]]> {
     const doubleLoop = (aMax: number, bMax: number, execute: (a: number, b: number) => void) => {
         var a: number;
         var b: number;
@@ -28,7 +26,7 @@ async function processFile(file: string): Promise<[string, number[]]> {
 
     doubleLoop(img.getWidth(), img.getHeight(), (w, h) => pixels.push(img.getPixelColor(w, h)));
 
-    console.log('Number of pixels: ' + pixels.length);
+    console.log(`${prefix}: Number of pixels: ${pixels.length}`);
 
     const uniq = _
         .chain(pixels)
@@ -39,33 +37,58 @@ async function processFile(file: string): Promise<[string, number[]]> {
         })
         .value();
 
-    console.log('Unique Number of pixels: ' + uniq.length);
+    console.log(`${prefix}: Unique Number of pixels: ${uniq.length}`);
 
     return [file, uniq];
 }
 
 async function main() {
-    const files = await <Promise<string[]>>new Promise((resolve, reject) => {
-        const basePath = path.join(__dirname, 'trim/content/14/jpeg/');
+    const relativeBase = path.resolve(path.join(__dirname, '../'));
+
+    const files = await (<Promise<string[]>>new Promise((resolve, reject) => {
+        const basePath = path.resolve(path.join(relativeBase, 'images/trim/content/14/jpeg/'));
+
+        const nonResourceForks = (x: string): boolean => x.match(/^(\._|\.DS_Store)/) === null;
+        const hasJpegExtension = (x: string): boolean => x.match(/\.jpe?g$/i) !== null;
 
         fs.readdir(basePath, function (err: NodeJS.ErrnoException, items: string[]) {
             if (!_.isNil(err)) {
                 reject(err);
             } else {
-                resolve(items.map((x) => path.join(basePath, x)));
+                resolve(items.filter(nonResourceForks).filter(hasJpegExtension).map((x) => path.join(basePath, x)));
             }
         });
+    })).then((x) => {
+        console.debug('Total Files: ', x.length);
+        return x;
     });
 
-    const output = await Promise.all(files.map(async (name) => await processFile(name)));
+    async function procSet(set: {items: string[], idx: number}): Promise<void> {
+        const actualI = set.idx + 1;
+        console.debug('Processing Set: ', actualI);
+        const output = await Promise.all(set.items.map(async (name, idx) => await processFile(`S${set.idx+1}/I${idx+1}`.padEnd(6, ' '), name)));
 
-    // NTS: File is a container for all images and refers to image index file...
-    fs.writeFileSync(
-        "./int_output.pb",
-        File
-            .encode(File.create(<IFile>{imgs: output.map((x) => Image.create(<IImage>{path: x[0], colors: x[1]}))}))
-            .finish());
+        // NTS: File is a container for all images and refers to image index file...
+        fs.writeFileSync(
+            path.resolve(path.join(relativeBase, `output_${actualI}.pb`)),
+            File
+                .encode(File.create(<IFile>{imgs: output.map((x) => Image.create(<IImage>{path: x[0], colors: x[1]}))}))
+                .finish());
+    }
 
+    function chunkThrottle<T>(array: T[], chunk: number, throttle: number, promiseFunction: (set:{items:T[], idx:number}[]) => void) {
+        _.chain(array)
+            .chunk(chunk)
+            .map((x, i)=>({items:x, idx:i}))
+            .chunk(throttle)
+            .value()
+            .reduce(async (previousPromise, nextItem) => {
+                await previousPromise;
+                return promiseFunction(nextItem);
+            }, Promise.resolve());
+    }
+
+    chunkThrottle(files, 25, 7, (sets) => Promise.all(sets.map(procSet)));
 }
 
 main();
