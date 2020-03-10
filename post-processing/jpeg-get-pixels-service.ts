323d64451da90396c7a182f5b213158af12d1718
Glenn R. Martin <grmartin@programmer.net>
2020-03-10 10:47:50 -0400

    Worker threads were totally worth it.

---
+++ post-processing/jpeg-get-pixels-service.ts
@@ -0,0 +1,75 @@
+import * as Jimp from 'jimp';
+import * as _ from 'lodash';
+import {Paths} from './shared/paths';
+import {File, IFile, IImage, Image, ISize} from './pbuff';
+import {ChunkSet} from './shared/chunking';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const basePath = Paths.getInputPath();
+
+const process_unique = true;
+
+const { workerData, parentPort } = require('worker_threads');
+
+async function procSet(set: ChunkSet<string>): Promise<void> {
+    return new Promise<void>((res) => {
+        const actualI = set.idx + 1;
+        console.debug('Processing Set: ', actualI);
+
+        Promise.all(set.items.map((name, idx) => processFile(`S${set.idx + 1}/I${idx + 1}`.padEnd(6, ' '), name)))
+            .then(output => {
+                // NTS: File is a container for all images and refers to image index file...
+                fs.writeFileSync(
+                    path.resolve(path.join(Paths.getOutputPath(), `${process_unique ? '' : 'actual_'}output_${actualI.toString().padStart(3, '0')}.pb`)),
+                    File
+                        .encode(File.create(<IFile>{
+                            rootPath: basePath,
+                            imgs: output.map((x) => Image.create(<IImage>{
+                                path: x[0].replace(basePath, '').replace(/^\/*/, ''),
+                                size: <ISize>{width: x[1], height: x[2]},
+                                colors: x[3]
+                            }))
+                        }))
+                        .finish());
+            })
+            .then(res);
+    });
+}
+
+async function processFile(prefix: string, file: string): Promise<[string, number, number, number[]]> {
+    const doubleLoop = (aMax: number, bMax: number, execute: (a: number, b: number) => void) => {
+        var a: number;
+        var b: number;
+        for (a = 0; a < aMax; a++) {
+            for (b = 0; b < bMax; b++) {
+                execute(a, b);
+            }
+        }
+    };
+
+    const img = await Jimp.read(file);
+    const pixels: number[] = [];
+
+    const toNumCode = (x: number) => {
+        const val = Jimp.intToRGBA(x);
+        return (val.r << 16) | (val.g << 8) | (val.b);
+    };
+
+    doubleLoop(img.getWidth(), img.getHeight(), (w, h) => pixels.push(toNumCode(img.getPixelColor(w, h))));
+
+    console.log(`${prefix}: Number of pixels: ${pixels.length}`);
+
+    let pixCode = pixels;
+
+    if (process_unique) {
+        pixCode = _.uniq(pixCode);
+        console.log(`${prefix}: Unique Number of pixels: ${pixCode.length}`);
+    }
+
+    return [file, img.getWidth(), img.getHeight(), pixCode];
+}
+
+procSet(workerData)
+    .then(() => parentPort.postMessage({ success: true }))
+    .catch((x: any) => parentPort.postMessage({success: false, data: x}));
--- post-processing/jpeg-get-pixels.ts
+++ post-processing/jpeg-get-pixels.ts
@@ -1,63 +1,15 @@
-import * as Jimp from 'jimp';
 import * as _ from 'lodash';
 import './shared/colors';
-import {IFile, IImage, File, Image, ISize} from './pbuff';
 import {Paths} from './shared/paths';
+import {ChunkSet, chunkThrottle} from './shared/chunking';
 const fs = require('fs');
 const path = require('path');
-const process_unique = false;
+const { Worker } = require('worker_threads');
 
-const NUM_CONCUR_SETS = 2;
+const NUM_CONCUR_SETS = 4;
 const NUM_PER_SET = 2;
 
 const START_TIME = new Date();
-
-async function processFile(prefix: string, file: string): Promise<[string, number, number, number[]]> {
-    const doubleLoop = (aMax: number, bMax: number, execute: (a: number, b: number) => void) => {
-        var a: number;
-        var b: number;
-        for (a = 0; a < aMax; a++) {
-            for (b = 0; b < bMax; b++) {
-                execute(a, b);
-            }
-        }
-    };
-
-    const img = await Jimp.read(file);
-    const pixels: number[] = [];
-
-    const toNumCode = (x: number) => {
-        const val = Jimp.intToRGBA(x);
-        return (val.r << 16) | (val.g << 8) | (val.b);
-    };
-
-    doubleLoop(img.getWidth(), img.getHeight(), (w, h) => pixels.push(toNumCode(img.getPixelColor(w, h))));
-
-    console.log(`${prefix}: Number of pixels: ${pixels.length}`);
-
-    let pixCode = pixels;
-
-    if (process_unique) {
-        pixCode = _.uniq(pixCode);
-        console.log(`${prefix}: Unique Number of pixels: ${pixCode.length}`);
-    }
-
-    return [file, img.getWidth(), img.getHeight(), pixCode];
-}
-
-type ChunkSet<T> = {items: T[], idx: number}
-
-function chunkThrottle<T>(array: T[], chunk: number, throttle: number, promiseFunction: (set:ChunkSet<T>[]) => void) {
-    _.chain(array)
-        .chunk(chunk)
-        .map((x, i)=>({items:x, idx:i}))
-        .chunk(throttle)
-        .value()
-        .reduce(async (previousPromise, nextItem) => {
-            await previousPromise;
-            return promiseFunction(nextItem);
-        }, Promise.resolve());
-}
 
 async function main() {
     const basePath = Paths.getInputPath();
@@ -82,36 +34,25 @@ async function main() {
         return x;
     });
 
-    async function procSet(set: ChunkSet<string>): Promise<void> {
-        return new Promise<void>((res) => {
-            const actualI = set.idx + 1;
-            console.debug('Processing Set: ', actualI);
-
-            Promise.all(set.items.map((name, idx) => processFile(`S${set.idx + 1}/I${idx + 1}`.padEnd(6, ' '), name)))
-                .then(output => {
-                    // NTS: File is a container for all images and refers to image index file...
-                    fs.writeFileSync(
-                        path.resolve(path.join(Paths.getOutputPath(), `${process_unique ? '' : 'actual_'}output_${actualI.toString().padStart(3, '0')}.pb`)),
-                        File
-                            .encode(File.create(<IFile>{
-                                rootPath: basePath,
-                                imgs: output.map((x) => Image.create(<IImage>{
-                                    path: x[0].replace(basePath, '').replace(/^\/*/, ''),
-                                    size: <ISize>{width: x[1], height: x[2]},
-                                    colors: x[3]
-                                }))
-                            }))
-                            .finish());
-                })
-                .then(res);
-        });
+    function runService(workerData: ChunkSet<string>) {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(`${__dirname}/jpeg-get-pixels-service.js`, { workerData });
+            worker.on('message', resolve);
+            worker.on('error', reject);
+            worker.on('exit', (code: number) => {
+                if (code !== 0)
+                    reject(new Error(`Worker stopped with exit code ${code}`));
+                else
+                    resolve();
+            })
+        })
     }
 
-    chunkThrottle(files, NUM_PER_SET, NUM_CONCUR_SETS, (sets) => Promise.all(sets.map(procSet)));
+    chunkThrottle(files, NUM_PER_SET, NUM_CONCUR_SETS, (sets) => Promise.all(sets.map(runService)));
 }
 
 process.on('beforeExit', (code) => {
-    console.debug(`Running Time: ${(START_TIME.getMilliseconds()/1000) - (new Date().getMilliseconds()/1000)} secs`);
+    console.debug(`Running Time: ${(new Date().getTime()/1000) - (START_TIME.getTime()/1000)} secs`);
     console.debug(`Terminating With Code: ${code}`);
 });
 
